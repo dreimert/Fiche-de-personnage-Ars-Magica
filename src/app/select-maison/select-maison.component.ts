de381d8f824ce86b0b3c2f903bb1fab589554adf
Damien Reimert <damien.reimert@gmail.com>
2017-02-11 14:36:03 +0100

    select-maison component

---
+++ src/app/select-maison/select-maison.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { Maison } from '../types/Maison';
+
+import { natures } from '../datas/natures';
+import { maisonAvantages as avantages, exMiscellaneaNature } from '../datas/maisonAvantages';
+
+import { enumToListe } from '../utiles/enumToListe';
+
+@Component({
+  selector: 'select-maison',
+  templateUrl: './select-maison.component.html',
+  styleUrls: ['./select-maison.component.css']
+})
+export class SelectMaisonComponent implements OnInit {
+  private _maison = null;
+  private _avantagesSelected = [];
+
+  @Output() maisonChange: EventEmitter<string> = new EventEmitter();
+  @Output() avantagesChange: EventEmitter<any> = new EventEmitter();
+
+  public readonly maisons = enumToListe(Maison);
+  public readonly avantages = avantages;
+
+  public readonly listVhm = natures.filter(exMiscellaneaNature.mineur.include, exMiscellaneaNature.mineur);
+  public readonly listVnhm = natures
+                             .filter(exMiscellaneaNature.majeur.est.include, exMiscellaneaNature.majeur.est)
+                             .filter(exMiscellaneaNature.majeur.nestpas.exclude, exMiscellaneaNature.majeur.nestpas);
+  public readonly listVihm = natures.filter(exMiscellaneaNature.vis.include, exMiscellaneaNature.vis);
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+  @Input()
+  get maison() {
+    return this._maison;
+  }
+
+  set maison(value) {
+    this._maison = value;
+    this.maisonChange.emit(value);
+
+    if(value === null) {
+      return;
+    }
+
+    if(avantages[this._maison].length === 1) {
+      this.avantagesSelected = this.avantages[this.maison][0].vertus;
+    } else {
+      this.avantagesSelected = null;
+    }
+  }
+
+  get avantagesSelected() {
+    return this._avantagesSelected[0];
+  }
+
+  set avantagesSelected(value) {
+    if(value === null) {
+      this._avantagesSelected = [];
+      this.avantagesChange.emit([value]);
+    } else {
+      this._avantagesSelected[0] = value;
+      this.avantagesChange.emit([value]);
+    }
+  }
+
+  @Input('avantages')
+  set avantagesInput(value) {
+    if(value === null) {
+      value = []
+    }
+
+    this._avantagesSelected = value;
+  }
+
+  get exMajeur() {
+    return this._avantagesSelected[1];
+  }
+
+  set exMajeur(value) {
+    this._avantagesSelected[1] = value;
+    this.avantagesChange.emit(this._avantagesSelected);
+  }
+
+  get exVis() {
+    return this._avantagesSelected[2];
+  }
+
+  set exVis(value) {
+    this._avantagesSelected[2] = value;
+    this.avantagesChange.emit(this._avantagesSelected);
+  }
+
+}
